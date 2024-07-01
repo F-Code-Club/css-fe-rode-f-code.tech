64addbf0923ddf3808fe52e71b59dfa92635291f
DangNgocThinhAn <thinhan1701@gmail.com>
2024-07-01 16:07:43 +0700

    Merge remote-tracking branch 'origin/main' into 3-dev-code-page-for-algorithm-rode

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
 const publicRuntimeConfig = {
     NODE_ENV: import.meta.env.NODE_ENV || 'production',
-    API_URL: import.meta.env.VITE_REACT_APP_BASE_URL,
-    API_AD_URL: import.meta.env.VITE_REACT_APP_V2_URL,
-    LOCAL_STORAGE_TOKEN: import.meta.env.VITE_REACT_APP_TOKEN_NAME,
+    API_URL: import.meta.env.VITE_REACT_APP_BASE_URL || "https://rode-be-socket.f-code.tech",
+    API_AD_URL: import.meta.env.VITE_REACT_APP_V2_URL || "https://rode-be.f-code.tech",
+    LOCAL_STORAGE_TOKEN: import.meta.env.VITE_REACT_APP_TOKEN_NAME || "token",
     USER_ROOM_ID: import.meta.env.VITE_REACT_APP_USER_ROOM_ID,
 };
 
